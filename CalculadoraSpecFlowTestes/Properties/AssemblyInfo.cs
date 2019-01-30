b7251cb70b37358ed1c629ba22d42f916b122e4b
Celso Aparecido Géa Bernar Junior <celso.bernar@programmers.com.br>
2019-01-30 13:22:30 -0200

    Add project files.

---
+++ CalculadoraSpecFlowTestes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculadoraSpecFlowTestes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculadoraSpecFlowTestes")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("508d77aa-f4dd-40cc-ae15-4830bfa3d72a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NetCoreCalculadoraTest/StepsImplementation/SomaSteps.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace NetCoreCalculadoraTest.StepsImplementation
+{
+    [Binding]
+    public class SomaSteps
+    {
+        private List<int> _arrSoma = new List<int>();
+        private int _result;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            _arrSoma.Add(p0);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            _result = _arrSoma.Sum();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(_result, p0);
+        }
+    }
+}
